43722ef92c9234239a3ccd6d8b30bdfae0eb6784
Anatiel Santos <anatielsantos@gmail.com>
2025-08-26 19:20:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,42 +9,31 @@ import random
 from contextlib import asynccontextmanager
 
 AMQP_URL = "amqp://bjnuffmq:gj-YQIiEXyfxQxjsZtiYDKeXIT8ppUq7@jaragua-01.lmq.cloudamqp.com/bjnuffmq"
-QUEUE_NAME = "fila.notificacao.entrada.anatielsantos"
-RETRY_QUEUE = "fila.notificacao.retry.anatielsantos"
-VALIDATION_QUEUE = "fila.notificacao.validacao.anatielsantos"
-DEAD_QUEUE = "fila.notificacao.dlq.anatielsantos"
+QUEUE_NAME = "notification.queue.input.anatielsantos"
+RETRY_QUEUE = "notification.queue.retry.anatielsantos"
+VALIDATION_QUEUE = "notification.queue.validation.anatielsantos"
+DEAD_QUEUE = "notification.queue.dlq.anatielsantos"
 
 connection = None
 channel = None
 
-notificacoes: dict[str, dict] = {}
-print(notificacoes)
+notifications: dict[str, dict] = {}
 
-
-class NotificacaoPayload(BaseModel):
-    mensagemId: UUID4 | None = None
-    conteudoMensagem: constr(min_length=1, max_length=1000)
-    tipoNotificacao: constr(min_length=1)
-
+class NotificationPayload(BaseModel):
+    message_id: UUID4 | None = None
+    message_content: constr(min_length=1, max_length=1000)
+    notification_type: constr(min_length=1)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    """
-    Gerencia ciclo de vida da aplicação
-    """
-
     global connection, channel
     params = pika.URLParameters(AMQP_URL)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
-    # entrada
     channel.queue_declare(queue=QUEUE_NAME, durable=True)
-    # retry
     channel.queue_declare(queue=RETRY_QUEUE, durable=True)
-    # validacao
     channel.queue_declare(queue=VALIDATION_QUEUE, durable=True)
-    # dlq
     channel.queue_declare(queue=DEAD_QUEUE, durable=True)
 
     yield
@@ -52,28 +41,22 @@ async def lifespan(app: FastAPI):
     if connection and connection.is_open:
         connection.close()
 
-
 app = FastAPI(lifespan=lifespan)
 
-
 @app.post("/api/notificar/")
-async def publish_message(payload: NotificacaoPayload):
-    """
-    Publica uma mensagem no RabbitMQ e gera e persiste um traceId em memória
-    """
-
+async def publish_message(payload: NotificationPayload):
     trace_id = str(uuid.uuid4())
-    mensagem_id = str(payload.mensagemId or uuid.uuid4())
+    message_id = str(payload.message_id or uuid.uuid4())
 
     message = {
         "traceId": trace_id,
-        "mensagemId": mensagem_id,
-        "conteudoMensagem": payload.conteudoMensagem,
-        "tipoNotificacao": payload.tipoNotificacao,
-        "status": "RECEBIDO"
+        "messageId": message_id,
+        "messageContent": payload.message_content,
+        "notificationType": payload.notification_type,
+        "status": "RECEIVED"
     }
 
-    notificacoes[trace_id] = message
+    notifications[trace_id] = message
 
     global channel
 
@@ -88,43 +71,32 @@ async def publish_message(payload: NotificacaoPayload):
         status_code=status.HTTP_202_ACCEPTED,
         content={
             "traceId": trace_id,
-            "mensagemId": mensagem_id,
-            "status": "Recebido para processamento assíncrono"
+            "messageId": message_id,
+            "status": "Accepted for asynchronous processing"
         }
     )
 
-
 @app.get("/api/notificar/{trace_id}")
-async def consultar_notificacao(trace_id: str):
-    """
-    Consulta uma notificação já registrada pelo traceId
-    """
-
-    notificacao = notificacoes.get(trace_id)
-    if not notificacao:
-        return {"error": "TraceId não encontrado"}
-    return notificacao
-
+async def get_notification(trace_id: str):
+    notification = notifications.get(trace_id)
+    if not notification:
+        return {"error": "TraceId not found"}
+    return notification
 
 @app.get("/api/consumer_one/")
 async def consume_message_one():
-    """
-    Consome uma mensagem da fila
-    """
-
     global channel
-
     method_frame, header_frame, body = channel.basic_get(queue=QUEUE_NAME, auto_ack=True)
     if not body:
-        return {"status": "fila vazia"}
+        return {"status": "queue empty"}
 
     message = json.loads(body.decode())
     trace_id = message.get("traceId")
 
-    falha_chance = random.randint(1, 100)
-    if falha_chance <= 15:
-        if trace_id in notificacoes:
-            notificacoes[trace_id]["status"] = "FALHA_PROCESSAMENTO_INICIAL"
+    failure_chance = random.randint(1, 100)
+    if failure_chance <= 15:
+        if trace_id in notifications:
+            notifications[trace_id]["status"] = "INITIAL_PROCESSING_FAILURE"
 
         channel.basic_publish(
             exchange="",
@@ -137,15 +109,13 @@ async def consume_message_one():
             status_code=400,
             content={
                 "traceId": trace_id,
-                "status": "FALHA_PROCESSAMENTO_INICIAL"
+                "status": "INITIAL_PROCESSING_FAILURE"
             }
         )
     else:
         await asyncio.sleep(random.uniform(1, 1.5))
-
-        if trace_id in notificacoes:
-            notificacoes[trace_id]["status"] = "PROCESSADO_INTERMEDIARIO"
-
+        if trace_id in notifications:
+            notifications[trace_id]["status"] = "INTERMEDIATE_PROCESSED"
             channel.basic_publish(
                 exchange="",
                 routing_key=VALIDATION_QUEUE,
@@ -162,25 +132,20 @@ async def consume_message_one():
 
 @app.get("/api/consumer_two/")
 async def consume_message_two():
-    """
-    Consome uma mensagem da fila
-    """
-
     global channel
-
     method_frame, header_frame, body = channel.basic_get(queue=RETRY_QUEUE, auto_ack=True)
     if not body:
-        return {"status": "fila vazia"}
-    
+        return {"status": "queue empty"}
+
     await asyncio.sleep(3)
 
     message = json.loads(body.decode())
     trace_id = message.get("traceId")
 
-    falha_chance = random.randint(1, 100)
-    if falha_chance <= 20:
-        if trace_id in notificacoes:
-            notificacoes[trace_id]["status"] = "FALHA_FINAL_REPROCESSAMENTO"
+    failure_chance = random.randint(1, 100)
+    if failure_chance <= 20:
+        if trace_id in notifications:
+            notifications[trace_id]["status"] = "FINAL_REPROCESSING_FAILURE"
 
         channel.basic_publish(
             exchange="",
@@ -193,13 +158,12 @@ async def consume_message_two():
             status_code=400,
             content={
                 "traceId": trace_id,
-                "status": "FALHA_FINAL_REPROCESSAMENTO"
+                "status": "FINAL_REPROCESSING_FAILURE"
             }
         )
     else:
-        if trace_id in notificacoes:
-            notificacoes[trace_id]["status"] = "REPROCESSADO_COM_SUCESSO"
-
+        if trace_id in notifications:
+            notifications[trace_id]["status"] = "REPROCESSED_SUCCESSFULLY"
             channel.basic_publish(
                 exchange="",
                 routing_key=VALIDATION_QUEUE,
@@ -216,79 +180,66 @@ async def consume_message_two():
 
 @app.get("/api/consumer_three/")
 async def consume_message_three():
-    """
-    Consome uma mensagem da fila
-    """
-
     global channel
-
     method_frame, header_frame, body = channel.basic_get(queue=VALIDATION_QUEUE, auto_ack=True)
     if not body:
-        return {"status": "fila vazia"}
-    
+        return {"status": "queue empty"}
+
     await asyncio.sleep(3)
 
     message = json.loads(body.decode())
     trace_id = message.get("traceId")
-    tipo = message.get("tipoNotificacao", "").upper()
+    notification_type = message.get("notificationType", "").upper()
 
-    falha_chance = random.randint(1, 100)
-    if falha_chance <= 5:
+    failure_chance = random.randint(1, 100)
+    if failure_chance <= 5:
         await asyncio.sleep(random.uniform(1, 1.5))
-
-        if trace_id in notificacoes:
-            notificacoes[trace_id]["status"] = "FALHA_ENVIO_FINAL"
-
+        if trace_id in notifications:
+            notifications[trace_id]["status"] = "FINAL_SEND_FAILURE"
             channel.basic_publish(
                 exchange="",
                 routing_key=DEAD_QUEUE,
                 body=json.dumps(message),
                 properties=pika.BasicProperties(delivery_mode=2)
             )
-
         return JSONResponse(
             status_code=400,
             content={
                 "traceId": trace_id,
-                "status": "FALHA_PROCESSAMENTO_INICIAL"
+                "status": "INITIAL_PROCESSING_FAILURE"
             }
         )
     else:
-        if trace_id in notificacoes:
-            notificacoes[trace_id]["status"] = "ENVIADO_SUCESSO"
+        if trace_id in notifications:
+            notifications[trace_id]["status"] = "SENT_SUCCESS"
 
-        if tipo == "EMAIL":
-            real_send_simulation = {"message": f"[EMAIL] Enviando email: {message['conteudoMensagem']}"}
-        elif tipo == "SMS":
-            real_send_simulation = {"message": f"[SMS] Enviando SMS: {message['conteudoMensagem']}"}
-        elif tipo == "PUSH":
-            real_send_simulation = {"message": f"[PUSH] Enviando notificação push: {message['conteudoMensagem']}"}
+        if notification_type == "EMAIL":
+            send_simulation = {"message": f"[EMAIL] Sending email: {message['messageContent']}"}
+        elif notification_type == "SMS":
+            send_simulation = {"message": f"[SMS] Sending SMS: {message['messageContent']}"}
+        elif notification_type == "PUSH":
+            send_simulation = {"message": f"[PUSH] Sending push notification: {message['messageContent']}"}
         else:
-            real_send_simulation = {"message": f"[DESCONHECIDO] Tipo inválido: {tipo}"}
+            send_simulation = {"message": f"[UNKNOWN] Invalid type: {notification_type}"}
 
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content=real_send_simulation
+        content=send_simulation
     )
 
 @app.get("/api/consumer_four/")
 async def consume_message_four():
-    """
-    Consome uma mensagem da fila
-    """
-
     global channel
-
     method_frame, header_frame, body = channel.basic_get(queue=DEAD_QUEUE, auto_ack=True)
     if not body:
-        return {"status": "fila vazia"}
-    
+        return {"status": "queue empty"}
+
     message = json.loads(body.decode())
 
     return JSONResponse(
         status_code=200,
         content={
             "content": message,
-            "message": "Esta mensagem não será mais processada"
+            "message": "This message will no longer be processed"
         }
     )
